7c8bc6692cbd616a8f5d710284e7354ce21f7c8b
CebEq <31326270+CebEq@users.noreply.github.com>
2021-08-07 17:00:07 -0500

    Prints

--- Cristian/cliente.py
+++ Cristian/cliente.py
@@ -9,32 +9,35 @@ socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5558")
 
 
-i=0
+i = 0
 while True:
 
 	# Inicio de nuevo ciclo
 	print("\n***Pidiendo hora...***")
 
 	#Tomamos el tiempo mr
-	mr=time.time()
+	mr = time.time()
 
-	#Modificamos este valor de forma "demostrativa" y comprobar el funcionamiento del algoritmo
-	prueba=random.randint(-5,12)
-	mr=mr+prueba
-	
+	#Modificamos este valor de forma "demostrativa" y comprobar el funcionamiento del algoritmo (para que no sea el mismo tiempo que el servidor)
+	prueba = random.randint(-5,12)
+	mr = mr + prueba	
 
 	#Enviamos el mensaje al servidor
 	socket.send_string(str(mr))
-	print("Hora actual = "+str(time.ctime(mr)))
+	#print("Hora actual = "+str(time.ctime(mr)))
 
 	#Recibimos el tiempo y calculamos el Tround que es el tiempo que tarda el mensaje en regresar
-	mt= float(socket.recv())
-	Tround= (time.time()+prueba)-mr
+	mt = float(socket.recv())
+	trecibido = time.time() + prueba
+	Tround = trecibido - mr
 
+	print("Tiempo de envío: "+str(time.ctime(mr)))
+	print("Tiempo de recepción: "+str(time.ctime(trecibido)))
 	print("Tround: "+str(Tround)+" [segundos]")
+	print()
 
 	#Calculamos la nueva hora con respecto al recibido y el Tround
-	t=mt+ (Tround/2)
+	t = mt + (Tround/2)
 	print("La hora recibida mt es: "+str(time.ctime(mt)))
 	print("La hora actualizada es: "+str(time.ctime(t)))
 
